03538e977d80072f516e0c4f5bffc6a1132aed4a
Teddy-Lang-11 <tlang2026@cchsdons.com>
2025-10-15 22:47:26 -0700

    Update InterestCalcMain.java

--- InterestCalcMain.java
+++ InterestCalcMain.java
@@ -1,14 +1,34 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.interestcalc;
+
+package com.mycompany.main;
 
 /**
  *
  * @author theod
  */
-public class Main {
-    
-}
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        //Set up scanner for user inputs
+         Scanner input = new Scanner(System.in);
+
+        // Ask user for starting amount
+         System.out.print("Enter the financial amount (e.g. 1000000): ");
+         double amount = input.nextDouble();
+
+        // Ask user for number of months
+         System.out.print("Enter number of months for repayment: ");
+         int months = input.nextInt();
+
+        // Create an object of InterestRateCalculator
+         InterestRateCalculator calculator = new InterestRateCalculator();
+
+        // Create method to calculate and display the payments
+         calculator.calculateRepayments(amount, months);
+
+        input.close();
+    }
+}
