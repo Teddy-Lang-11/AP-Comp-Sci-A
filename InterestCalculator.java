14e3ace12cf170643cc052a9d0d8b6fa577e103d
Teddy-Lang-11 <tlang2026@cchsdons.com>
2025-10-15 22:44:36 -0700

    Add files via upload

---
+++ InterestCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interestcalc;
+
+/**
+ *
+ * @author theod
+ */
+public class InterestCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
