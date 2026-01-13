c41f95ced356c3bd8b5b9742c16937da1a8b80b5
Teddy-Lang-11 <tlang2026@cchsdons.com>
2026-01-13 14:16:19 -0800

    Add files via upload

---
+++ SortPlayers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sortplayers;
+
+/**
+ *
+ * @author TLang2026
+ */
+public class SortPlayers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
