8964352dcbca08d3b887923f3b21c340a9cf4235
Teddy-Lang-11 <tlang2026@cchsdons.com>
2026-02-11 12:42:24 -0800

    Add files via upload

---
+++ ClassInheritance.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classinheritance;
+
+/**
+ *
+ * @author TLang2026
+ */
+
+/* DIRECTIONS:
+I want you to use the code I have supplied for Vehicle and Car, and expand on this. 
+Look up vehicle specs on the web.
+
+I want each person to create 2 Class files that inherit from Car, as well as 2 Boat Classes, 
+2 Airplane classes, and 2 other vehicle Classes (impress me!). I basicallty want 
+to get you used to using inheritance.
+
+You can override the toString to print out speial characteristics, use the Speedometer methods 
+as needed (should all speeds be in mph?). I may give extra credit if I am impressed by someone's 
+imaginitive coding. Please make sure to comment everything well.
+*/
+
+
+public class ClassInheritance {
+
+    public static void main(String[] args) {
+        
+        // plug in vehicle class
+            //now utilize car class
+                //2 cars
+            //plane class
+                //2 planes
+            //boat class
+                //2 boats
+            //Other Class
+            
+    }
+}
