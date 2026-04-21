03d5072dff5e06c3b70ef5e979e21c3fb7a4c680
Teddy-Lang-11 <tlang2026@cchsdons.com>
2026-04-21 09:08:45 -0700

    Update SemesterTwoGameProject.java

--- SemesterTwoGameProject.java
+++ SemesterTwoGameProject.java
@@ -4,6 +4,8 @@
  */
 package semestertwogameproject;
 
+import java.util.Random;
+
 /**
  *
  * @author TLang2026
@@ -13,8 +15,39 @@ public class SemesterTwoGameProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        /*
+            Start Game
+                3 saves
+                    if old one 
+                        load it
+                    if new one
+                        ask name (set name for new save & interactions)
+                        ask money you wish to deposit (set initail bank account balance)
+                Start Game
+                    ask for bet amount
+                        set bet
+                    ask for auto spin
+                        if yes
+                            ask for ending condition
+                                minimun money balance OR not enough to meet bet
+                        if no
+                            it's ok
+                    Win Cases
+                        if 3 of 
+                        
+        */
+        
+        
+    
+    }
 }
+//-------NEW-CLASS-------------------------------------------
+ 
+//-------NEW-CLASS-------------------------------------------  
+  
+//-------NEW-CLASS-------------------------------------------  
+    
+    
+
